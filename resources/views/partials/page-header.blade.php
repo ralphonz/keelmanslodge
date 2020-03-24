f69722d8e52fd755011bfcb4cf9a94f91c2dd29b
Ralph Stokes <ralph@blueleafstudio.net>
2020-03-24 11:27:28 +0000

    1.1.5 - Initial Bug Fixes
    Fix: CSS - footer brand nowrap
    Fix: CSS - Scroll down chevron in hero not clickable

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -2,7 +2,7 @@
   @if (has_post_thumbnail())
     <div class="hero full-width-section jarallax" data-jarallax data-speed="0.2">
       {{ the_post_thumbnail(null, ['class' => 'jarallax-img']) }}
-      <a href="#page-title">@include('icons.down-chevron')</a>
+      <a href="#page-title" class="scroll-down-to-page">@include('icons.down-chevron')</a>
     </div>
   @endif
   <h1 id="page-title">{!! App::title() !!}</h1>
